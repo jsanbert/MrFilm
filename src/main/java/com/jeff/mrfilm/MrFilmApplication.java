f353d36f28ae6ac43bf8b33e73da1ff78205f725
Jesús Sánchez <jsbertolin@gmail.com>
2019-11-28 10:46:40 +0100

    First commit: project creation

---
+++ src/main/java/com/jeff/mrfilm/MrFilmApplication.java
@@ -0,0 +1,13 @@
+package com.jeff.mrfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrFilmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrFilmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeff/mrfilm/MrFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeff.mrfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrFilmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
