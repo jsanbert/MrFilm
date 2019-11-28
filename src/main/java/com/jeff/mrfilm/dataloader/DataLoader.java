021d9bef2ea14ed85a7f4808d6fbb52f498b2939
Jesús Sánchez <jsbertolin@gmail.com>
2019-11-28 11:36:37 +0100

    Create data loader class to insert test data

---
+++ src/main/java/com/jeff/mrfilm/dataloader/DataLoader.java
@@ -0,0 +1,45 @@
+package com.jeff.mrfilm.dataloader;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    /*
+    DECLARE REPOS
+
+    private Repo1 repo1;
+    private Repo2 repo2;
+    ...
+    */
+
+    @Autowired
+    public DataLoader(/* Repo1 repo1, Repo2 repo2 ... */) {
+        /*
+
+        INITIALIZE REPOS
+
+        this.repo1 = repo1;
+        this.repo2 = repo2;
+        ...
+
+        */
+    }
+
+    public void run(ApplicationArguments args) {
+        /*
+        INSERT ITEMS
+
+        ItemRepo1 item = new ItemRepo1(.....);
+        repo1.insert(item);
+        ...
+
+         */
+    }
+}
