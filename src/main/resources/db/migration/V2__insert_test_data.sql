afdb5298d7200ffee71f651b5f57a3b21c96e076
Jesús Sánchez <jsbertolin@gmail.com>
2019-12-09 13:22:32 +0100

    Fix flyway support

--- src/main/resources/db/migration/V2__insert_test_data.sql
+++ src/main/resources/db/migration/V2__insert_test_data.sql
@@ -1,9 +1,9 @@
-START TANSACTION;
+START TRANSACTION;
 INSERT INTO `actors` (`id`, `birth_date`, `name`, `surname`, `country_id`) VALUES
-(7, '1947-07-30', 'Arnold', 'Schwarzenegger', 5),
+(7, '1947-07-29', 'Arnold', 'Schwarzenegger', 5),
 (8, '1946-07-05', 'Sylvester', 'Stallone', 3),
 (9, '1960-05-24', 'Richard', 'Crenna', 1),
-(10, '1938-07-08', 'Bryan', 'Dennehy', 1),
+(10, '1938-07-09', 'Bryan', 'Dennehy', 1),
 (11, '1956-07-31', 'Michael', 'Bienne', 2),
 (12, '1956-09-26', 'Linda', 'Hamilton', 2);
 
@@ -15,9 +15,13 @@ INSERT INTO `countries` (`id`, `code`, `name`) VALUES
 (5, 'AT', 'Austria'),
 (6, 'UK', 'United Kingdom');
 
+INSERT INTO `directors` (`id`, `birth_date`, `name`, `surname`, `country_id`) VALUES
+(13, '1954-08-16', 'James', 'Cameron', 1),
+(14, '1931-07-07', 'Ted', 'Kotcheff', 4);
+
 INSERT INTO `films` (`id`, `premiere_year`, `prizes_won`, `rate`, `synopsis`, `title`, `country_id`, `director`) VALUES
 (21, 1982, 1, 8.8, 'A veteran Green Beret is forced by a cruel Sheriff and his deputies to flee into the mountains and wage an escalating one-man war against his pursuers.', 'Rambo', 1, 14),
-(22, 1997, 6, 7.5, 'In 1984, a human soldier is tasked to stop an indestructible cyborg killing machine, both sent from 2029, from executing a young woman, whose unborn son is the key to humanity''s future salvation.', 'Terminator', 1, 13);
+(22, 1997, 6, 7.5, 'In 1984, a human soldier is tasked to stop an indestructible cyborg killing machine, both sent from 2029, from executing a young woman, whose unborn son is the key to humanity\'s future salvation.', 'Terminator', 1, 13);
 
 INSERT INTO `films_actors` (`film_id`, `actor_id`) VALUES
 (21, 8),
@@ -42,5 +46,11 @@ INSERT INTO `genres` (`id`, `name`) VALUES
 (18, 'Romantic'),
 (19, 'War'),
 (20, 'Sci-Fi');
+
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+(23),
+(23),
+(23),
+(23);
 
 COMMIT;
